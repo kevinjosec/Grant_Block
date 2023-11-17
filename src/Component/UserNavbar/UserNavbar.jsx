efc30efdb3277d3da6120c847243969ec3c29dc2
Kevin <kevinjosecheriachery@gmail.com>
2023-11-18 02:53:09 +0530

    nov 1823

--- src/Component/UserNavbar/UserNavbar.jsx
+++ src/Component/UserNavbar/UserNavbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './UserNavbar.css'
 import { CgProfile } from "react-icons/cg";
 import { useNavigate } from 'react-router-dom';
 import {auth} from '../../firebase'
 function UserNavbar() {
   const navigate = useNavigate();
+
+  const [popUp, setPopUp] = useState('');
+
+  const popUpRef = useRef(null);
+
+  const togglePopUp = setPopUp(!popUp)
+
   const handleSignOut =()=>{
     try{
       auth.signOut();
@@ -14,11 +21,24 @@ function UserNavbar() {
         console.log(err);
       }
     }
+
+  const profileSettings =()=>{
+    <button className='profileSettings'>Profile Setings</button>
+  }
+
+  useEffect(()=>{const handleClick = (event)=>{
+    if(popUpRef.current && !popUpRef.current.contains(event.target)){
+      setPopUp(false);
+    }
+  };document.addEventListener('click',handleClick);
+  return ()=>{document.removeEventListener('click',handleClick);};
+
+  },[])
   
   return (
     <div>
         <div className="userNavbar">
-          Grant Block <CgProfile className="userProfile" onClick={handleSignOut}></CgProfile>
+          Grant Block <CgProfile className="userProfile" onClick={profileSettings}></CgProfile>
           <ul className="userNavbarList">
             <li className='li' onClick={()=>navigate('/UserHomePage')}>
               Home
